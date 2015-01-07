8a3a3aab5bf222d5835c989827376169e2d94481
Edward Price <ted.price@gmail.com>
2015-01-07 12:19:25 -0600

    Call backs and what have you

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,34 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+
+    # Find user one way
+    user = User.find_by(provider: access_token.provider, uid: access_token.uid)
+
+    # No dice? Find user another way
+    if user.nil?
+      user = User.find_by(:email => access_token.info.email)
+    end
+
+    # If no user found, create user via Google info
+    if user.nil?
+      user = create_user_from_access_token(access_token)
+    end
+
+    user
+  end
+
+  private 
+
+  def self.create_user_from(access_token)
+    data = access_token.info
+    User.create(name: data["name"],
+                provider: access_token.provider,
+                email: data["email"],
+                uid: access_token.uid ,
+                password: Devise.friendly_token[0,20],
+               )
+  end
 end
